6dd78f69007c206a2368424a94ae77b154cba443
sanbragone <bragon@gmail.com>
2022-08-03 21:40:27 -0300

    Update archivos

--- js/test.js
+++ js/test.js
@@ -1,18 +1,46 @@
-function valorFinal(precio, porcentajeImpuesto, porcentajeDescuento) {
-  return (
-    precio +
-    (precio * porcentajeImpuesto) / 100 -
-    (precio * porcentajeDescuento) / 100
-  );
+const precios = [
+  {
+    symbol: "ETHBTC",
+    price: "0.07051000",
+  },
+  {
+    symbol: "LTCBTC",
+    price: "0.00254700",
+  },
+  {
+    symbol: "BNBBTC",
+    price: "0.01290300",
+  },
+  {
+    symbol: "NEOBTC",
+    price: "0.00047300",
+  },
+  {
+    symbol: "BCCBTC",
+    price: "0.07908100",
+  },
+  {
+    symbol: "GASBTC",
+    price: "0.00012600",
+  },
+  {
+    symbol: "BNBETH",
+    price: "0.18300000",
+  },
+  {
+    symbol: "BTCUSDT",
+    price: "23451.20000000",
+  },
+  {
+    symbol: "ETHUSDT",
+    price: "1653.50000000",
+  },
+];
+
+for (let i = 0; i < precios.length; i++) {
+  console.log(precios[i].price);
 }
 
-for (let i = 0; i < 3; i++) {
-  let precioFinal = valorFinal(
-    parseFloat(prompt("Ingresar precio del producto")),
-    parseFloat(prompt("Ingresar % de impuesto")),
-    parseFloat(prompt("Ingresar % de descuento"))
-  );
-
-  alert("El precio final del producto es: $" + precioFinal);
-  console.log("Precio final: " + precioFinal);
+for (const price of precios) {
+  console.log(price.price);
 }
